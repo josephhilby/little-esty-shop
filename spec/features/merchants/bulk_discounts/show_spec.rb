a89d540431282d918cefaadf854d8a90bd22ef26
Amanda Ross <amikaross@gmail.com>
2022-11-11 09:27:26 -0700

    Feature: Create bulk items show page

---
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "Merchant Bulk Discount Show page" do 
+  before(:each) do 
+    @merchant = Merchant.create!(name: "Savory Spice")
+    @discount = @merchant.bulk_discounts.create!(discount: 15, threshold: 5)
+  end
+  describe "As a merchant, when I visit /merchants/:id/bulk_discounts/:id" do 
+    it "displays the bulk discount's quantity threshold and percentage discount" do 
+      visit merchant_bulk_discount_path(@merchant, @discount)
+save_and_open_page
+      expect(page).to have_content("Quantity Threshold: 5 items")
+      expect(page).to have_content("Percentage Discount: 15%")
+    end
+  end
+end
